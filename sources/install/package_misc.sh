8ea40a104b28e464b03f66cab57dc520035387ba
taylor-craig <89658043+taylor-craig@users.noreply.github.com>
2024-10-03 12:07:35 -0500

    Update package_misc.sh

--- sources/install/package_misc.sh
+++ sources/install/package_misc.sh
@@ -21,14 +21,14 @@ function install_misc_apt_tools() {
     add-to-list "rsync,https://packages.debian.org/sid/rsync,File synchronization tool for efficiently copying and updating data between local or remote locations"
 }
 
-function install_goshs() {
+
+function install_croc() {
     # CODE-CHECK-WHITELIST=add-aliases
-    colorecho "Installing goshs"
-    go install -v github.com/patrickhener/goshs@latest
-    asdf reshim golang
-    add-history goshs
-    add-test-command "goshs -v"
-    add-to-list "goshs,https://github.com/patrickhener/goshs,Goshs is a replacement for Python's SimpleHTTPServer. It allows uploading and downloading via HTTP/S with either self-signed certificate or user provided certificate and you can use HTTP basic auth."
+    colorecho "Installing Croc"
+    curl https://getcroc.schollz.com | bash
+    add-history croc
+    add-test-command "croc --version"
+    add-to-list "croc,https://getcroc.schollz.com,Croc is a tool that allows any two computers to simply and securely transfer files and folders."
 }
 
 function install_shellerator() {
@@ -38,15 +38,6 @@ function install_shellerator() {
     add-history shellerator
     add-test-command "shellerator --help"
     add-to-list "shellerator,https://github.com/ShutdownRepo/Shellerator,a simple command-line tool for generating shellcode"
-}
-
-function install_uberfile() {
-    # CODE-CHECK-WHITELIST=add-aliases
-    colorecho "Installing uberfile"
-    pipx install --system-site-packages git+https://github.com/ShutdownRepo/uberfile
-    add-history uberfile
-    add-test-command "uberfile --help"
-    add-to-list "uberfile,https://github.com/ShutdownRepo/Uberfile,Uberfile is a simple command-line tool aimed to help pentesters quickly generate file downloader one-liners in multiple contexts (wget / curl / powershell / certutil...). This project code is based on my other similar project for one-liner reverseshell generation Shellerator."
 }
 
 function install_arsenal() {
@@ -67,44 +58,6 @@ function install_whatportis() {
     add-history whatportis
     add-test-command "whatportis --version"
     add-to-list "whatportis,https://github.com/ncrocfer/whatportis,Command-line tool to lookup port information"
-}
-
-function install_searchsploit() {
-    # CODE-CHECK-WHITELIST=add-aliases
-    colorecho "Installing searchsploit"
-    if [[ ! -d "/opt/tools/exploitdb" ]]
-    then
-        git -C /opt/tools/ clone --depth 1 https://gitlab.com/exploit-database/exploitdb
-        add-history searchsploit
-        add-test-command "searchsploit --help; searchsploit --help |& grep 'You can use any number of search terms'"
-        add-to-list "searchsploit,https://gitlab.com/exploit-database/exploitdb,A command line search tool for Exploit-DB"
-    else
-        colorecho "Searchsploit is already installed"
-    fi
-    ln -sf /opt/tools/exploitdb/searchsploit /opt/tools/bin/searchsploit
-    cp -n /opt/tools/exploitdb/.searchsploit_rc ~/
-    sed -i 's/\(.*[pP]aper.*\)/#\1/' ~/.searchsploit_rc
-    sed -i 's/opt\/exploitdb/opt\/tools\/exploitdb/' ~/.searchsploit_rc
-}
-
-function install_trilium() {
-    colorecho "Installing Trilium (building from sources)"
-    # TODO : apt install in a second step
-    fapt libpng16-16 libpng-dev pkg-config autoconf libtool build-essential nasm libx11-dev libxkbfile-dev
-    git -C /opt/tools/ clone -b stable --depth 1 https://github.com/zadam/trilium.git
-    zsh -c "source ~/.zshrc && cd /opt/tools/trilium && nvm install 16 && nvm use 16 && npm install && npm rebuild && npm run webpack"
-    mkdir -p /root/.local/share/trilium-data
-    # config.ini contains the exposition port and host
-    cp -v /root/sources/assets/trilium/config.ini /root/.local/share/trilium-data
-    cp -v /root/sources/assets/trilium/trilium-manager.sh /opt/tools/trilium/trilium-manager.sh
-    chmod +x /opt/tools/trilium/trilium-manager.sh
-    zsh /opt/tools/trilium/trilium-manager.sh start
-    zsh /opt/tools/trilium/trilium-manager.sh configure
-    zsh /opt/tools/trilium/trilium-manager.sh stop
-    add-aliases trilium
-    add-history trilium
-    add-test-command "trilium-test"
-    add-to-list "trilium,https://github.com/zadam/trilium,Personal knowledge management system."
 }
 
 function install_ngrok() {
@@ -128,54 +81,6 @@ function install_ngrok() {
     add-to-list "ngrok,https://github.com/inconshreveable/ngrok,Expose a local server behind a NAT or firewall to the internet"
 }
 
-function install_objectwalker() {
-    # CODE-CHECK-WHITELIST=add-aliases
-    colorecho "Installing objectwalker"
-    pipx install --system-site-packages git+https://github.com/p0dalirius/objectwalker
-    add-history objectwalker
-    add-test-command "objectwalker --help"
-    add-to-list "objectwalker,https://github.com/p0dalirius/objectwalker,A python module to explore the object tree to extract paths to interesting objects in memory."
-}
-
-function install_tig() {
-    # CODE-CHECK-WHITELIST=add-aliases,add-history
-    colorecho "Installing tig"
-    git -C /opt/tools clone --depth 1 https://github.com/jonas/tig.git
-    cd /opt/tools/tig || exit
-    make
-    make install
-    mv /root/bin/tig /opt/tools/bin/tig
-    # Need add-history ?
-    add-test-command "tig --help"
-    add-to-list "tig,https://github.com/jonas/tig,Tig is an ncurses-based text-mode interface for git."
-}
-
-function install_yt-dlp() {
-    # CODE-CHECK-WHITELIST=add-aliases,add-history
-    colorecho "Installing yt-dlp"
-    pipx install --system-site-packages git+https://github.com/yt-dlp/yt-dlp
-    add-test-command "yt-dlp --help"
-    add-to-list "yt-dlp,https://github.com/yt-dlp/yt-dlp,A youtube-dl fork with additional features and fixes"
-}
-
-function install_cyberchef() {
-    # CODE-CHECK-WHITELIST=add-aliases,add-history
-    colorecho "Installing CyberChef"
-    local last_release
-    last_release=$(curl --location --silent "https://api.github.com/repos/gchq/CyberChef/releases/latest"|grep browser_download_url|awk '{print $2}'|tr -d '"')
-    echo "$last_release"
-    if [[ -z "$last_release" ]]; then
-        criticalecho-noexit "Latest release not found" && return
-    fi
-    mkdir /opt/tools/CyberChef
-    wget "$last_release" -O /tmp/CyberChef.zip
-    unzip -o /tmp/CyberChef.zip -d /opt/tools/CyberChef/
-    rm /tmp/CyberChef.zip
-    mv /opt/tools/CyberChef/CyberChef_*.html /opt/tools/CyberChef/CyberChef.html
-    add-test-command "file /opt/tools/CyberChef/CyberChef.html"
-    add-to-list "CyberChef,https://github.com/gchq/CyberChef/,The Cyber Swiss Army Knife"
-}
-
 function install_creds() {
     # CODE-CHECK-WHITELIST=add-aliases
     colorecho "Installing creds"
@@ -192,18 +97,11 @@ function package_misc() {
     local end_time
     start_time=$(date +%s)
     install_misc_apt_tools
-    install_goshs           # Web uploader/downloader page
-    install_searchsploit    # Exploitdb local search engine
+    install_croc
     install_shellerator     # Reverse shell generator
-    install_uberfile        # file uploader/downloader commands generator
     install_arsenal         # Cheatsheets tool
-    install_trilium         # notes taking tool
     install_ngrok           # expose a local development server to the Internet
     install_whatportis      # Search default port number
-    install_objectwalker    # Python module to explore the object tree to extract paths to interesting objects in memory
-    install_tig             # ncurses-based text-mode interface for git
-    install_yt-dlp          # A youtube-dl fork with additional features and fixes
-    install_cyberchef       # A web based toolbox
     install_creds           # A default credentials vault
     end_time=$(date +%s)
     local elapsed_time=$((end_time - start_time))
