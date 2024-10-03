cf9c6c9a27e58a43c0053f1da7f377ef2f5214b8
taylor-craig <89658043+taylor-craig@users.noreply.github.com>
2024-10-03 12:10:48 -0500

    Update package_c2.sh

--- sources/install/package_c2.sh
+++ sources/install/package_c2.sh
@@ -5,17 +5,6 @@ source common.sh
 # sourcing package_ad.sh for the install_powershell() function
 source package_ad.sh
 
-function install_pwncat() {
-    # CODE-CHECK-WHITELIST=add-aliases
-    colorecho "Installing pwncat"
-    pipx install --system-site-packages pwncat-cs
-    # Because Blowfish has been deprecated, downgrade cryptography version - https://github.com/paramiko/paramiko/issues/2038
-    pipx inject pwncat-cs cryptography==36.0.2
-    add-history pwncat
-    add-test-command "pwncat-cs --version"
-    add-to-list "pwncat,https://github.com/calebstewart/pwncat,A lightweight and versatile netcat alternative that includes various additional features."
-}
-
 function install_metasploit() {
     # CODE-CHECK-WHITELIST=add-history
     colorecho "Installing Metasploit"
@@ -50,15 +39,6 @@ function install_metasploit() {
     add-to-list "metasploit,https://github.com/rapid7/metasploit-framework,A popular penetration testing framework that includes many exploits and payloads"
 }
 
-function install_routersploit() {
-    # CODE-CHECK-WHITELIST=add-history
-    colorecho "Installing RouterSploit"
-    pipx install --system-site-packages routersploit
-    pipx inject routersploit colorama
-    add-aliases routersploit
-    add-test-command "routersploit --help"
-    add-to-list "routersploit,https://github.com/threat9/routersploit,Security audit tool for routers."
-}
 
 function install_sliver() {
     # CODE-CHECK-WHITELIST=add-aliases
@@ -85,44 +65,6 @@ function install_sliver() {
     add-to-list "sliver,https://github.com/BishopFox/sliver,Open source / cross-platform and extensible C2 framework"
 }
 
-function install_empire() {
-    colorecho "Installing Empire"
-    wget -O /tmp/packages-microsoft-prod.deb https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb
-    dpkg -i /tmp/packages-microsoft-prod.deb
-    fapt apt-transport-https libicu-dev xclip zip
-    # Installing .NET 6.0 SDK
-    wget -O /tmp/dotnet-install.sh https://dot.net/v1/dotnet-install.sh
-    chmod +x /tmp/dotnet-install.sh
-    /tmp/dotnet-install.sh --channel 6.0
-    install_powershell
-    git -C /opt/tools/ clone --recursive https://github.com/BC-SECURITY/Empire
-    cd /opt/tools/Empire || exit
-    python3 -m venv --system-site-packages ./venv
-    source ./venv/bin/activate
-    if [[ $(uname -m) = 'x86_64' ]]
-    then
-      pip3 install .
-    elif [[ $(uname -m) = 'aarch64' ]]
-    then
-      # for ARM64, pip install doesn't work because of donut-shellcode not supporting this arch (https://github.com/TheWover/donut/issues/139)
-#      criticalecho-noexit "This installation function doesn't support architecture $(uname -m)" && return
-      pip3 install .
-    else
-      criticalecho-noexit "This installation function doesn't support architecture $(uname -m)" && return
-    fi
-    deactivate
-    # TODO : use mysql instead, need to configure that
-    sed -i 's/use: mysql/use: sqlite/g' empire/server/config.yaml
-    sed -i 's/password: password123/password: exegol4thewin/g' empire/server/config.yaml
-    cp -r -v ./empire/server/data/Invoke-Obfuscation /opt/tools/powershell/7/Modules/
-    add-aliases empire
-    add-history empire
-    add-test-command "ps-empire server --help"
-    add-test-command "ps-empire client --help"
-    add-to-list "empire,https://github.com/BC-SECURITY/Empire,post-exploitation and adversary emulation framework"
-    # exit the Empire workdir, since it sets the python version to 3.12 and could mess up later installs
-    cd || exit
-}
 
 function install_havoc() {
     colorecho "Installing Havoc"
@@ -145,19 +87,6 @@ function install_havoc() {
     add-to-list "Havoc,https://github.com/HavocFramework/Havoc,Command & Control Framework"
 }
 
-function install_villain() {
-    colorecho "Installing Villain"
-    git -C /opt/tools/ clone --depth 1 https://github.com/t3l3machus/Villain
-    cd /opt/tools/Villain || exit
-    python3 -m venv --system-site-packages ./venv
-    source ./venv/bin/activate
-    pip3 install -r ./requirements.txt
-    deactivate
-    add-aliases villain
-    add-history villain
-    add-test-command "Villain.py -h"
-    add-to-list "Villain,https://github.com/t3l3machus/Villain,Command & Control Framework"
-}
 
 # Package dedicated to command & control frameworks
 function package_c2() {
@@ -165,13 +94,9 @@ function package_c2() {
     local start_time
     local end_time
     start_time=$(date +%s)
-    install_empire                  # Post-ex and adversary simulation framework
-    install_pwncat                  # netcat and rlwrap on steroids to handle revshells, automates a few things too
     install_metasploit              # Offensive framework
-    install_routersploit            # Exploitation Framework for Embedded Devices
     install_sliver                  # Sliver is an open source cross-platform adversary emulation/red team framework
     install_havoc                   # C2 in Go
-    install_villain                 # C2 using hoaxShell in Python
     end_time=$(date +%s)
     local elapsed_time=$((end_time - start_time))
     colorecho "Package c2 completed in $elapsed_time seconds."
